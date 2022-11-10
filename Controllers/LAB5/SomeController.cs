be1538b2d8200ba222b4f0bea9d8611985e41836
Vortex784 <109910240+Vortex784@users.noreply.github.com>
2022-11-10 11:13:14 +0100

    lab5

---
+++ Controllers/LAB5/SomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Services;
+using WebApplication3.Services.LAB5;
+
+
+namespace WebApplication3.Controllers.LAB5
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SomeController : ControllerBase
+    {
+        [HttpGet]
+       public string CallMethod()
+        {
+            var someInstance = new SomeClass();
+            return someInstance.Print(null);
+        }
+
+    }
+}
---
+++ Services/LAB5/SomeClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+namespace WebApplication3.Services.LAB5
+{
+    public class SomeClass
+
+    {
+       public string Print(string text)
+        {
+            try
+            {
+                return text.Length.ToString();
+            }
+            catch (Exception ex)
+            {
+                return ex.StackTrace;
+            }
+        }
+    }
+}
